61f025d676a275ca795ba9cf5d83409bd6101bb2
Gabriel Jodison <gabriel_jodison@outlook.com.br>
2024-01-31 15:46:40 -0300

    finish commit

---
+++ js/index.js
@@ -0,0 +1,130 @@
+const buttonSearch = document.querySelector('.buttonSearch');
+const inputValue = document.querySelector('.inputValue');
+const tableTitle = document.querySelector('#trTitle');
+const tableContent = document.querySelector('#trNew');
+const info = document.querySelector('.table-container');
+
+
+
+buttonSearch.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let inputValueResponse = inputValue.value;
+
+
+    inputValueResponse = inputValueResponse.replace(" ", "");
+    inputValueResponse = inputValueResponse.replace(".", "");
+    inputValueResponse = inputValueResponse.trim();
+
+
+    axios
+        .get(`https://viacep.com.br/ws/${inputValueResponse}/json/`)
+        .then(function(response){
+
+            
+            createTextTd(response.data.logradouro);
+            createTextTd(response.data.bairro );
+            createTextTd(response.data.localidade + '/' + response.data.uf);
+            createTextTd(response.data.cep );
+
+
+            createTitle('Rua');
+            createTitle('Bairro');
+            createTitle('Cidade');
+            createTitle('CEP');
+            
+
+           
+    });
+
+})
+
+function createTitle(responseCep){
+    let createTitleElement = document.createElement('td');
+    let createTextTitle = document.createTextNode(responseCep);
+
+    createTitleElement.appendChild(createTextTitle);
+    tableTitle.appendChild(createTitleElement);
+
+
+}
+
+
+function createTextTd(responseCep){
+    let createElementTd = document.createElement('td');
+    let createTextTd = document.createTextNode(responseCep);
+
+
+    createElementTd.appendChild(createTextTd);
+    tableContent.appendChild(createElementTd)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let inputCep = document.getElementById('cep');
+// let rua = document.getElementById('rua');
+// let bairro = document.getElementById('bairro');
+// let cidade = document.getElementById('cidade');
+
+// // cep.value = '07175570';
+
+// inputCep.addEventListener('blur', function(){
+//     let cep = inputCep.value;
+
+//     if(cep.length !== 8){
+//         return;
+//     }
+
+//     fetch(`viacep.com.br/ws/${cep}/json/`)
+//     .then(response => response.json())
+//     .then(json => {
+
+//         rua.value = json.rua;
+//         bairro.value = json.bairro;
+//         cidade.value = json.cidade;
+//     });
+
+  
+// })
+
